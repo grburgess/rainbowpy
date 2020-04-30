2f0de0b83dfd27aef448f59b839adc5ff9d8cc4a
grburgess <jmichaelburgess@gmail.com>
2020-04-30 15:06:59 +0200

    added new file

---
+++ rainbowpy/generate_maps_from_config.py
@@ -0,0 +1,29 @@
+import os
+
+import yaml
+
+from rainbowpy.colormap import ColorMap
+from rainbowpy.file_utils import (file_existing_and_readable,
+                                  get_path_of_user_dir,
+                                  if_directory_not_existing_then_make)
+
+
+def add_custom_cmaps():
+
+    if_directory_not_existing_then_make(get_path_of_user_dir())
+
+    user_maps_file = os.path.join(get_path_of_user_dir(), "custom_maps.yml")
+
+    if file_existing_and_readable(user_maps_file):
+
+        with open(user_maps_file, "r") as f:
+
+            cmaps = yaml.load(f, Loader=yaml.SafeLoader)
+            
+            
+        for map_name, colors in cmaps.items():
+
+            print(f"{map_name}")
+            
+            ColorMap.from_hex(list(colors.keys()),
+                              discrete=False, name=map_name)
